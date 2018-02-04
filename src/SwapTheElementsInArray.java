6f2e2d5d71bca2b0a803881de92f7e844ebb2e2c
Denis Nikashin <denis0n@ukr.net>
2018-02-04 23:58:42 +0200

    second commit. Added five methods for work with loops by sort the arrays

--- src/SwapTheElementsInArray.java
+++ src/SwapTheElementsInArray.java
@@ -7,8 +7,16 @@ public class SwapTheElementsInArray {
         System.out.println( Arrays.toString(dataElements) + ": initial version ");
         //fun(dataElements);
         //fun_1(dataElements);
-       // fun_2(dataElements);
-       fun_3(dataElements);
+        //fun_2(dataElements);
+        //fun_3(dataElements);
+        //fun_4(dataElements);
+        //fun_5(dataElements);
+        //fun_6(dataElements);
+        //fun_7(dataElements);
+        //fun_8(dataElements);
+        //fun_9(dataElements);
+        //fun_10(dataElements);
+        fun_11(dataElements);
     }
 
     /*цикл для перемещения элемента из ячейки с нулевым индексом в ячейку с индексом [data.length-1] */
@@ -24,16 +32,90 @@ public class SwapTheElementsInArray {
             swap(data, k, k - 1);
         }
     }
+
     /*2-й вариант цикла для перемещения элемента из ячейки с индексом [data.length-1] в ячейку с нулевым индексом */
     static void fun_2(int[] data) {
         for (int k = 1; k < data.length; k++) {
             swap(data, 0, k);
         }
     }
+
     /*3-й вариант цикла для перемещения элемента из ячейки с индексом [data.length-1] в ячейку с нулевым индексом */
     static void fun_3(int[] data) {
         for (int k = 0; k < data.length - 1; k++) {
             swap(data, k, data.length - 1);
+        }
+    }
+
+    /*цикл для перемещения минимального элемента в конец массива */
+    static void fun_4(int[] data) {
+        for (int k = 0; k < data.length - 1; k++) {
+            if (data[k] < data[k + 1]) {
+                swap(data, k, k + 1);
+            }
+        }
+    }
+
+    /*цикл для перемещения минимального элемента в начало массива */
+    static void fun_5(int[] data) {
+        for (int k = data.length - 1; k > 0; k--) {
+            if (data[k] < data[k - 1]) {
+                swap(data, k, k - 1);
+            }
+        }
+    }
+
+    /*цикл для перемещения максимального элемента в конец массива */
+    static void fun_6(int[] data) {
+        for (int k = 0; k < data.length - 1; k++) {
+            if (data[k] > data[k + 1]) {
+                swap(data, k, k + 1);
+            }
+        }
+    }
+
+    /*1-й вариант цикла для перемещения максимального элемента в начало массива */
+    static void fun_7(int[] data) {
+        for (int k = data.length - 1; k > 0; k--) {
+            if (data[k] > data[k - 1]) {
+                swap(data, k, k - 1);
+            }
+        }
+    }
+
+    /*2-й вариант цикла для перемещения максимального элемента в начало массива */
+    static void fun_8(int[] data) {
+        for (int k = 1; k < data.length; k++) {
+            if (data[0] < data[k]) {
+                swap(data, 0, k);
+            }
+        }
+    }
+
+    /*цикл для перемещения минимального элемента в начало массива */
+    static void fun_9(int[] data) {
+        for (int k = 1; k < data.length; k++) {
+            if (data[0] > data[k]) {
+                swap(data, 0, k);
+            }
+        }
+    }
+
+    /*цикл для перемещения минимального элемента в конец массива */
+    static void fun_10(int[] data) {
+        for (int k = 0; k < data.length - 1; k++) {
+            if (data[k] < data[data.length - 1]) {
+                swap(data, k, data.length - 1);
+            }
+        }
+    }
+
+    /*цикл для перемещения максимального элемента в конец массива */
+    static void fun_11(int[] data) {
+        for (int k = 0; k < data.length - 1; k++) {
+            if (data[k] > data[data.length - 1]) {
+                swap(data, k, data.length - 1);
+            }
         }
     }
 
