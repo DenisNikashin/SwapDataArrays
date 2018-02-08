ab1caa339932141b9948858df18f5fe8d679e19d
Denis Nikashin <denis0n@ukr.net>
2018-02-09 00:56:46 +0200

    added several methods

--- src/SwapTheElementsInArray.java
+++ src/SwapTheElementsInArray.java
@@ -16,7 +16,13 @@ public class SwapTheElementsInArray {
         //fun_8(dataElements);
         //fun_9(dataElements);
         //fun_10(dataElements);
-        fun_11(dataElements);
+        //fun_11(dataElements);
+        //swap(dataElements, 1, 4);
+        //fun_12(dataElements);
+        //fun_13(dataElements, 10);
+        fun_14(dataElements, 10);
+
+
     }
 
     /*цикл для перемещения элемента из ячейки с нулевым индексом в ячейку с индексом [data.length-1] */
@@ -119,11 +125,60 @@ public class SwapTheElementsInArray {
         }
     }
 
-    /*код для обмена значениями */
+    /*код для обмена значениями, значения меняются только у тех ячеек чьи значения индексов
+      * совпадают со значениями которые переданы методу */
     private static void swap(int[] data, int src, int dest) {
         int temp = dataElements[src];
         dataElements[src] = dataElements[dest];
         dataElements[dest] = temp;
         System.out.println(Arrays.toString(data));
+    }
+
+    /*инвертирование массива*/
+    private static void fun_12(int[] array) {
+        for (int k = 0; k < array.length / 2; k++) {
+            int tmp = array[k];
+            array[k] = array[array.length - 1 - k];
+            array[array.length - 1 - k] = tmp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    /*алгоритм для линейного поиска индекса ячейки в котором находиться искомое число,
+    * если число не найдено выведиться на консоль просто массив*/
+    private static int fun_13(int[] array, int x) {
+        for (int k = 0; k < array.length; k++) {
+            if (array[k] == x) {
+                System.out.println("индекс ячейки в []  в которой находиться число " + x + " --> " + k);
+                return k;
+            }
+            System.out.println(k);
+        }
+
+        System.out.println(Arrays.toString(array));
+        return -1;
+    }
+
+    /*алгоритм бинарного поиска*/
+    static int fun_14(int[] array, int x) {
+        int low = 0;
+            System.out.println("low -> " + low);
+        int high = array.length - 1;
+            System.out.println("high -> " + high);
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            System.out.println("mid --> " + mid);
+            int midVal = array[mid];
+            if (midVal < x)
+                low = mid + 1;
+            else if (midVal > x)
+                high = mid - 1;
+            else
+
+                return mid;
+        }
+        System.out.println(Arrays.toString(array));
+        return -(low + 1);
     }
 }
